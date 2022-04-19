b5e6e81768b1a9e3679f7736362ad6eb865dcc16
Andrey Kruglov <kruglov.it@gmail.com>
2022-04-19 21:02:00 +0300

    telemetry completed

--- Makefile
+++ Makefile
@@ -15,3 +15,12 @@ security:
 
 expand:
 	cargo expand
+
+udeps:
+	cargo +nightly udeps --all-targets
+
+# We are using the `bunyan` CLI to prettify the outputted logs
+# The original `bunyan` requires NPM, but you can install a Rust-port with
+# `cargo install bunyan`
+test:
+	TEST_LOG=true cargo test health_check_works | bunyan
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,17 +9,31 @@ pub struct FormData {
     name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
-    let request_id = Uuid::new_v4();
-    log::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
-    );
-    log::info!("request_id {} - Saving new subscriber details in the database", request_id);
+    match insert_subscriber(&pool, &form).await
+    {
+        Ok(_) => HttpResponse::Ok(),
+        Err(_) => HttpResponse::InternalServerError()
+    }
+}
 
-    match sqlx::query!(
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    form: &FormData
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -29,16 +43,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> im
         form.name,
         Utc::now()
     )
-    .execute(pool.get_ref())
-    .await
-    {
-        Ok(_) => {
-            log::info!("request_id {} - New subscriber details have been saved", request_id);
-            HttpResponse::Ok()
-        }
-        Err(e) => {
-            log::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
-            HttpResponse::InternalServerError()
-        }
-    }
+        .execute(pool)
+        .await
+        .map_err(|e| {
+            tracing::error!("Failed to execute query: {:?}", e);
+            e
+        })?;
+
+    Ok(())
 }
