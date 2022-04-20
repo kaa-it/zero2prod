04a7dee3d365141cd6349e7ed59d5f904a227705
Andrey Kruglov <kruglov.it@gmail.com>
2022-04-20 21:18:26 +0300

    invalid subscribers 1 completed

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,3 +1,6 @@
+use crate::domain::NewSubscriber;
+use crate::domain::SubscriberEmail;
+use crate::domain::SubscriberName;
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
@@ -5,8 +8,8 @@ use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
-    email: String,
-    name: String,
+  email: String,
+  name: String,
 }
 
 #[tracing::instrument(
@@ -18,33 +21,48 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    match insert_subscriber(&pool, &form).await {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish(),
-    }
+  let name = match SubscriberName::parse(form.0.name) {
+    Ok(name) => name,
+    Err(_) => return HttpResponse::BadRequest().finish(),
+  };
+
+  let email = match SubscriberEmail::parse(form.0.email) {
+    Ok(email) => email,
+    Err(_) => return HttpResponse::BadRequest().finish(),
+  };
+
+  let new_subscriber = NewSubscriber { email, name };
+
+  match insert_subscriber(&pool, &new_subscriber).await {
+    Ok(_) => HttpResponse::Ok().finish(),
+    Err(_) => HttpResponse::InternalServerError().finish(),
+  }
 }
 
 #[tracing::instrument(
-    name = "Saving new subscriber details in the database",
-    skip(form, pool)
+  name = "Saving new subscriber details in the database",
+  skip(new_subscriber, pool)
 )]
-pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
-    sqlx::query!(
-        r#"
+pub async fn insert_subscriber(
+  pool: &PgPool,
+  new_subscriber: &NewSubscriber,
+) -> Result<(), sqlx::Error> {
+  sqlx::query!(
+    r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
         "#,
-        Uuid::new_v4(),
-        form.email,
-        form.name,
-        Utc::now()
-    )
-    .execute(pool)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
+    Uuid::new_v4(),
+    new_subscriber.email.as_ref(),
+    new_subscriber.name.as_ref(),
+    Utc::now()
+  )
+  .execute(pool)
+  .await
+  .map_err(|e| {
+    tracing::error!("Failed to execute query: {:?}", e);
+    e
+  })?;
 
-    Ok(())
+  Ok(())
 }
