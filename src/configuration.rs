8da65b24be07366c0bb93e7fb20423dd385f5c2e
Andrey Kruglov <kruglov.it@gmail.com>
2022-04-22 03:36:51 +0300

    pending confirmation completed

--- src/configuration.rs
+++ src/configuration.rs
@@ -5,116 +5,122 @@ use sqlx::postgres::{PgConnectOptions, PgSslMode};
 use sqlx::ConnectOptions;
 use std::convert::TryInto;
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Clone)]
 pub struct Settings {
-  pub database: DatabaseSettings,
-  pub application: ApplicationSettings,
-  pub email_client: EmailClientSettings,
+    pub database: DatabaseSettings,
+    pub application: ApplicationSettings,
+    pub email_client: EmailClientSettings,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Clone)]
 pub struct ApplicationSettings {
-  #[serde(deserialize_with = "deserialize_number_from_string")]
-  pub port: u16,
-  pub host: String,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub port: u16,
+    pub host: String,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Clone)]
 pub struct DatabaseSettings {
-  pub username: String,
-  pub password: Secret<String>,
-  #[serde(deserialize_with = "deserialize_number_from_string")]
-  pub port: u16,
-  pub host: String,
-  pub database_name: String,
-  pub require_ssl: bool,
+    pub username: String,
+    pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+    pub require_ssl: bool,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Clone)]
 pub struct EmailClientSettings {
-  pub base_url: String,
-  pub sender_email: String,
-  pub authorization_token: Secret<String>,
+    pub base_url: String,
+    pub sender_email: String,
+    pub authorization_token: Secret<String>,
+    pub timeout_milliseconds: u64,
 }
 
 impl EmailClientSettings {
-  pub fn sender(&self) -> Result<SubscriberEmail, String> {
-    SubscriberEmail::parse(self.sender_email.clone())
-  }
+    pub fn sender(&self) -> Result<SubscriberEmail, String> {
+        SubscriberEmail::parse(self.sender_email.clone())
+    }
+
+    pub fn timeout(&self) -> std::time::Duration {
+        std::time::Duration::from_millis(self.timeout_milliseconds)
+    }
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-  let mut settings = config::Config::default();
+    let mut settings = config::Config::default();
 
-  let base_path = std::env::current_dir().expect("Failed ton determine the current directory");
-  let configuration_directory = base_path.join("configuration");
+    let base_path = std::env::current_dir().expect("Failed ton determine the current directory");
+    let configuration_directory = base_path.join("configuration");
 
-  settings.merge(config::File::from(configuration_directory.join("base")).required(true))?;
+    settings.merge(config::File::from(configuration_directory.join("base")).required(true))?;
 
-  let environment: Environment = std::env::var("APP_ENVIRONMENT")
-    .unwrap_or_else(|_| "local".into())
-    .try_into()
-    .expect("Failed to parse APP_ENVIRONMENT.");
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
 
-  settings
-    .merge(config::File::from(configuration_directory.join(environment.as_str())).required(true))?;
+    settings.merge(
+        config::File::from(configuration_directory.join(environment.as_str())).required(true),
+    )?;
 
-  // Add in settings from environment variables (with a prefix of APP and '__' as separator)
-  // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
-  settings.merge(config::Environment::with_prefix("app").separator("__"))?;
+    // Add in settings from environment variables (with a prefix of APP and '__' as separator)
+    // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
+    settings.merge(config::Environment::with_prefix("app").separator("__"))?;
 
-  settings.try_into()
+    settings.try_into()
 }
 
 impl DatabaseSettings {
-  pub fn with_db(&self) -> PgConnectOptions {
-    let mut options = self.without_db().database(&self.database_name);
-    options.log_statements(tracing::log::LevelFilter::Trace);
-    options
-  }
+    pub fn with_db(&self) -> PgConnectOptions {
+        let mut options = self.without_db().database(&self.database_name);
+        options.log_statements(tracing::log::LevelFilter::Trace);
+        options
+    }
 
-  pub fn without_db(&self) -> PgConnectOptions {
-    let ssl_mode = if self.require_ssl {
-      PgSslMode::Require
-    } else {
-      PgSslMode::Prefer
-    };
+    pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            PgSslMode::Prefer
+        };
 
-    PgConnectOptions::new()
-      .host(&self.host)
-      .username(&self.username)
-      .password(&self.password.expose_secret())
-      .port(self.port)
-      .ssl_mode(ssl_mode)
-  }
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(&self.password.expose_secret())
+            .port(self.port)
+            .ssl_mode(ssl_mode)
+    }
 }
 
 /// The possible runtime environment for our application.
 pub enum Environment {
-  Local,
-  Production,
+    Local,
+    Production,
 }
 
 impl Environment {
-  pub fn as_str(&self) -> &'static str {
-    match self {
-      Environment::Local => "local",
-      Environment::Production => "production",
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
     }
-  }
 }
 
 impl TryFrom<String> for Environment {
-  type Error = String;
+    type Error = String;
 
-  fn try_from(s: String) -> Result<Self, Self::Error> {
-    match s.to_lowercase().as_str() {
-      "local" => Ok(Self::Local),
-      "production" => Ok(Self::Production),
-      other => Err(format!(
-        "{} is not a supported environment. Use either `local` or `production`.",
-        other
-      )),
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production`.",
+                other
+            )),
+        }
     }
-  }
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use crate::domain::NewSubscriber;
 use crate::domain::SubscriberEmail;
 use crate::domain::SubscriberName;
+use crate::email_client::EmailClient;
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
@@ -8,64 +9,99 @@ use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
-  email: String,
-  name: String,
+    email: String,
+    name: String,
 }
 
 impl TryFrom<FormData> for NewSubscriber {
-  type Error = String;
+    type Error = String;
 
-  fn try_from(value: FormData) -> Result<Self, Self::Error> {
-    let name = SubscriberName::parse(value.name)?;
-    let email = SubscriberEmail::parse(value.email)?;
-    Ok(Self { email, name })
-  }
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { email, name })
+    }
 }
 
 #[tracing::instrument(
     name = "Adding a new subscriber",
-    skip(form, pool),
+    skip(form, pool, email_client),
     fields(
         subscriber_email = %form.email,
         subscriber_name = %form.name
     )
 )]
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-  let new_subscriber = match form.0.try_into() {
-    Ok(form) => form,
-    Err(_) => return HttpResponse::BadRequest().finish(),
-  };
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+    email_client: web::Data<EmailClient>,
+) -> HttpResponse {
+    let new_subscriber = match form.0.try_into() {
+        Ok(form) => form,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
 
-  match insert_subscriber(&pool, &new_subscriber).await {
-    Ok(_) => HttpResponse::Ok().finish(),
-    Err(_) => HttpResponse::InternalServerError().finish(),
-  }
+    if insert_subscriber(&pool, &new_subscriber).await.is_err() {
+        return HttpResponse::InternalServerError().finish();
+    }
+
+    if send_confirmation_email(&email_client, new_subscriber)
+        .await
+        .is_err()
+    {
+        return HttpResponse::InternalServerError().finish();
+    }
+
+    HttpResponse::Ok().finish()
 }
 
 #[tracing::instrument(
-  name = "Saving new subscriber details in the database",
-  skip(new_subscriber, pool)
+    name = "Saving new subscriber details in the database",
+    skip(new_subscriber, pool)
 )]
 pub async fn insert_subscriber(
-  pool: &PgPool,
-  new_subscriber: &NewSubscriber,
+    pool: &PgPool,
+    new_subscriber: &NewSubscriber,
 ) -> Result<(), sqlx::Error> {
-  sqlx::query!(
-    r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+        VALUES ($1, $2, $3, $4, 'pending_confirmation')
         "#,
-    Uuid::new_v4(),
-    new_subscriber.email.as_ref(),
-    new_subscriber.name.as_ref(),
-    Utc::now()
-  )
-  .execute(pool)
-  .await
-  .map_err(|e| {
-    tracing::error!("Failed to execute query: {:?}", e);
-    e
-  })?;
+        Uuid::new_v4(),
+        new_subscriber.email.as_ref(),
+        new_subscriber.name.as_ref(),
+        Utc::now()
+    )
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
 
-  Ok(())
+    Ok(())
+}
+
+#[tracing::instrument(
+    name = "Send a confirmation email to a new subscriber",
+    skip(email_client, new_subscriber)
+)]
+pub async fn send_confirmation_email(
+    email_client: &EmailClient,
+    new_subscriber: NewSubscriber,
+) -> Result<(), reqwest::Error> {
+    let confirmation_link = "https://my-api.com/subscriptions/confirm";
+    let plain_body = format!(
+        "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+        confirmation_link
+    );
+    let html_body = format!(
+        "Welcome to our newsletter!<br />\
+Click <a href=\"{}\">here</a> to confirm your subscription.",
+        confirmation_link
+    );
+    email_client
+        .send_email(new_subscriber.email, "Welcome!", &html_body, &plain_body)
+        .await
 }
